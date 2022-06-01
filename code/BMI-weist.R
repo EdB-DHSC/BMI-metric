0df4949989e3b3113511b604519e1a63c90ada91
Beake <Edward.Beake@dhsc.gov.uk>
2022-06-01 12:12:40 +0100

    add code to read in and tidy BMI vs waist HSE data

--- code/BMI-weist.R
+++ code/BMI-weist.R
@@ -5,11 +5,20 @@ library(tidyxl)
 install.packages("tidyverse")
 library(tidyverse)
 
-hse_waist <- xlsx_cells("data/HSE19-Overweight-obesity-tab.xlsx",
-                        sheets = 'Table 11')
+hse_waist <- xlsx_cells("data/HSE19-Overweight-obesity-tab-waist.xlsx")
 
-hse_waist %>% filter(row != 1,
+hse_waist_formats <- xlsx_formats("data/HSE19-Overweight-obesity-tab-waist.xlsx")
+
+tidy <- hse_waist %>% filter(row != 1,
                      row != 2,
                      row != 5,
-                     col != 2) %>% 
-  behead('up',header1)
+                     col != 2,
+                     !is_blank) %>% 
+  behead('up-left',header1)%>% 
+  behead('up',year) %>% 
+  behead_if(hse_waist_formats$local$font$bold[local_format_id] == TRUE,
+            direction = "left-up",
+            name = "BMI_cat") %>%
+  behead('left','waist') %>% 
+  select(numeric,year,BMI_cat,waist) %>% 
+  filter(!is.na(waist))
