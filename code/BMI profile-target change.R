b1095099f0469e489b9bc0fef9c2bb89d4dd514f
Beake <Edward.Beake@dhsc.gov.uk>
2022-06-15 11:21:36 +0100

    It initial commit of code to show the change in BMI distribution to meet AOT targets

---
+++ code/BMI profile-target change.R
@@ -0,0 +1,101 @@
+install.packages("tidyverse")
+library(tidyverse)
+library(haven)
+
+HSE2017 <- read_sav("C:/Users/EBeake/OneDrive - Department of Health and Social Care/Data/HSfE/2017/UKDA-8488-spss/spss/spss24/hse17i_eul_v1.sav") %>% 
+  filter(BMIok == 1,
+         Age16g5 >= 2,
+         Sex >= 1) %>% 
+  select(BMI,Age16g5,Sex,qimd,wt_int,whval) %>% 
+  mutate(BMI_cat = case_when(BMI <= 18.5 ~ 'underweight',
+                             BMI >= 18.5 & BMI < 25 ~ 'healthy weight',
+                             BMI >= 25 & BMI < 30 ~ 'overweight',
+                             BMI >= 30 & BMI < 40 ~ 'obese',
+                             BMI >= 40 ~ 'sever obese'),
+         BMI_rounded = round(BMI,0))
+
+HSE2016 <- read_sav("C:/Users/EBeake/OneDrive - Department of Health and Social Care/Data/HSfE/2016/UKDA-8334-spss/spss/spss19/hse2016_eul.sav") %>% 
+  filter(BMIok == 1,
+         Age16g5 >= 2,
+         Sex >= 1) %>% 
+  select(BMI,Age16g5,Sex,qimd,wt_int,whval) %>% 
+  mutate(BMI_cat = case_when(BMI <= 18.5 ~ 'underweight',
+                             BMI >= 18.5 & BMI < 25 ~ 'healthy weight',
+                             BMI >= 25 & BMI < 30 ~ 'overweight',
+                             BMI >= 30 & BMI < 40 ~ 'obese',
+                             BMI >= 40 ~ 'sever obese'),
+         BMI_rounded = round(BMI,0))
+
+HSE2015 <- read_sav("C:/Users/EBeake/OneDrive - Department of Health and Social Care/Data/HSfE/2015/UKDA-8280-spss/spss/spss19/hse2015ai.sav") %>% 
+  filter(BMIok == 1,
+         Age16g5 >= 2,
+         Sex >= 1) %>% 
+  select(BMI,Age16g5,Sex,qimd,wt_int,whval) %>% 
+  mutate(BMI_cat = case_when(BMI <= 18.5 ~ 'underweight',
+                             BMI >= 18.5 & BMI < 25 ~ 'healthy weight',
+                             BMI >= 25 & BMI < 30 ~ 'overweight',
+                             BMI >= 30 & BMI < 40 ~ 'obese',
+                             BMI >= 40 ~ 'sever obese'),
+         BMI_rounded = round(BMI,0))
+
+ HSE <- bind_rows(HSE2017,
+                 HSE2016,
+                 HSE2015)
+
+HSE %>% filter(BMI <= 50) %>% 
+  mutate(stabalise = BMI_rounded - 1.5,
+         third = BMI_rounded - 7,
+         fith = BMI_rounded - 4.5,
+         quarter = BMI_rounded - 5,
+         BMI_cat = case_when(BMI_rounded <= 18.5 ~ 'underweight',
+                             BMI_rounded >= 18.5 & BMI_rounded < 25 ~ 'healthy weight',
+                             BMI_rounded >= 25 & BMI_rounded < 30 ~ 'overweight',
+                             BMI_rounded >= 30 & BMI_rounded < 40 ~ 'obese',
+                             BMI_rounded >= 40 ~ 'sever obese')) %>% 
+  pivot_longer(cols = c('BMI_rounded','stabalise','third','fith','quarter'),
+               names_to = 'target',
+               values_to = 'BMI_rounded') %>% 
+  group_by(BMI_rounded,target) %>% 
+  summarise(count = n(),wt = mean(wt_int)) %>% 
+  mutate(count = count * wt) %>% 
+  ggplot(aes(BMI_rounded,count,col = target)) +
+  geom_line() +
+  geom_vline(xintercept = 18.5, linetype = 2)+
+  geom_vline(xintercept = 25, linetype = 2)+
+  geom_vline(xintercept = 30, linetype = 2)+
+  geom_vline(xintercept = 40, linetype = 2)+
+  theme_bw()+
+labs(title = 'BMI Profile of Adults in England',
+     subtitle = 'BMI of over 18"s in HSE 15-17',
+     y = '',
+     x = 'BMI') +
+  theme(axis.text.y = element_blank()) 
+
+HSE %>% mutate(qimd = as.numeric(qimd),
+               Sex = case_when(Sex == 1 ~ 'Male',
+                               Sex == 2 ~ 'Female')) %>% 
+  filter(BMI <= 50,
+               qimd == 1 |
+                 qimd == 5) %>% 
+  mutate(Deprevation = case_when(qimd == 1 ~ 'Least Deprived',
+                                   qimd == 5 ~ 'Most Deprived')) %>% 
+  group_by(BMI_rounded,Deprevation,Sex) %>% 
+  summarise(count = n(),wt = mean(wt_int)) %>% 
+  mutate(count = count * wt,
+         count = case_when(Deprevation == 'Least Deprived' & Sex == 'Female' ~ count * (2000/2004),
+                           Deprevation == 'Least Deprived' & Sex == 'Male' ~ count * (2000/1841),
+                           Deprevation == 'Most Deprived' & Sex == 'Female' ~ count * (2000/1852),
+                           Deprevation == 'Most Deprived' & Sex == 'Male' ~ count * (2000/1827))) %>% 
+  ggplot(aes(BMI_rounded,count,col=Deprevation)) +
+  geom_line() +
+  geom_vline(xintercept = 18.5, linetype = 2)+
+  geom_vline(xintercept = 25, linetype = 2)+
+  geom_vline(xintercept = 30, linetype = 2)+
+  geom_vline(xintercept = 40, linetype = 2) +
+  theme_bw()+
+  facet_wrap(~Sex) +
+  labs(title = 'BMI Profile of Adults in England by Depravation and Gender',
+       subtitle = 'BMI of over 18"s in HSE 15-17',
+       y = '',
+       x = 'BMI') +
+  theme(axis.text.y = element_blank()) 
