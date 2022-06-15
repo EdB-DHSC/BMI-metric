be38dbab8f2c194f6532c265cdf57abe5afcb926
Beake <Edward.Beake@dhsc.gov.uk>
2022-06-15 12:13:28 +0100

    BMI change altered so it is with respect to baseline and colour and naming aligned to the slides

--- code/BMI profile-target change.R
+++ code/BMI profile-target change.R
@@ -43,21 +43,25 @@ HSE2015 <- read_sav("C:/Users/EBeake/OneDrive - Department of Health and Social 
                  HSE2015)
 
 HSE %>% filter(BMI <= 50) %>% 
-  mutate(stabalise = BMI_rounded - 1.5,
-         third = BMI_rounded - 7,
-         fith = BMI_rounded - 4.5,
-         quarter = BMI_rounded - 5,
+  mutate(third = BMI_rounded - 6,
+         fith = BMI_rounded - 3.5,
+         quarter = BMI_rounded - 4.5,
          BMI_cat = case_when(BMI_rounded <= 18.5 ~ 'underweight',
                              BMI_rounded >= 18.5 & BMI_rounded < 25 ~ 'healthy weight',
                              BMI_rounded >= 25 & BMI_rounded < 30 ~ 'overweight',
                              BMI_rounded >= 30 & BMI_rounded < 40 ~ 'obese',
                              BMI_rounded >= 40 ~ 'sever obese')) %>% 
-  pivot_longer(cols = c('BMI_rounded','stabalise','third','fith','quarter'),
+  pivot_longer(cols = c('BMI_rounded','third','fith','quarter'),
                names_to = 'target',
                values_to = 'BMI_rounded') %>% 
   group_by(BMI_rounded,target) %>% 
   summarise(count = n(),wt = mean(wt_int)) %>% 
-  mutate(count = count * wt) %>% 
+  mutate(count = count * wt,
+         target = str_replace(target,'third','Reduce adult obesity rates by a third by 2035'),
+         target = str_replace(target,'BMI_rounded','Current'),
+#         target = str_replace(target,'stabalise','Halt the rise in adult obesity rates by 2030'),
+         target = str_replace(target,'fith','Reduce adult obesity rates by a fith by 2035'),
+         target = str_replace(target,'quarter','Reduce adult obesity rates by a quarter by 2035')) %>%
   ggplot(aes(BMI_rounded,count,col = target)) +
   geom_line() +
   geom_vline(xintercept = 18.5, linetype = 2)+
@@ -66,36 +70,14 @@ HSE %>% filter(BMI <= 50) %>%
   geom_vline(xintercept = 40, linetype = 2)+
   theme_bw()+
 labs(title = 'BMI Profile of Adults in England',
-     subtitle = 'BMI of over 18"s in HSE 15-17',
+ #    subtitle = "BMI of over 18's in HSE 2015-17",
      y = '',
      x = 'BMI') +
-  theme(axis.text.y = element_blank()) 
-
-HSE %>% mutate(qimd = as.numeric(qimd),
-               Sex = case_when(Sex == 1 ~ 'Male',
-                               Sex == 2 ~ 'Female')) %>% 
-  filter(BMI <= 50,
-               qimd == 1 |
-                 qimd == 5) %>% 
-  mutate(Deprevation = case_when(qimd == 1 ~ 'Least Deprived',
-                                   qimd == 5 ~ 'Most Deprived')) %>% 
-  group_by(BMI_rounded,Deprevation,Sex) %>% 
-  summarise(count = n(),wt = mean(wt_int)) %>% 
-  mutate(count = count * wt,
-         count = case_when(Deprevation == 'Least Deprived' & Sex == 'Female' ~ count * (2000/2004),
-                           Deprevation == 'Least Deprived' & Sex == 'Male' ~ count * (2000/1841),
-                           Deprevation == 'Most Deprived' & Sex == 'Female' ~ count * (2000/1852),
-                           Deprevation == 'Most Deprived' & Sex == 'Male' ~ count * (2000/1827))) %>% 
-  ggplot(aes(BMI_rounded,count,col=Deprevation)) +
-  geom_line() +
-  geom_vline(xintercept = 18.5, linetype = 2)+
-  geom_vline(xintercept = 25, linetype = 2)+
-  geom_vline(xintercept = 30, linetype = 2)+
-  geom_vline(xintercept = 40, linetype = 2) +
-  theme_bw()+
-  facet_wrap(~Sex) +
-  labs(title = 'BMI Profile of Adults in England by Depravation and Gender',
-       subtitle = 'BMI of over 18"s in HSE 15-17',
-       y = '',
-       x = 'BMI') +
-  theme(axis.text.y = element_blank()) 
+  theme(axis.text.y = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.direction = 'vertical',
+        legend.position = 'bottom') +
+  scale_color_manual(values=c("black",
+                              "#B39CE1",
+                              "#F188F5",
+                              "red"))
